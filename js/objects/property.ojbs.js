bcf495145d2973bece555937fa2d34c7b8ac4011
Karan <xkaranx5@gmail.com>
2025-02-18 13:02:39 +0530

    obj prop

--- js/objects/property.ojbs.js
+++ js/objects/property.ojbs.js
@@ -1,3 +1,19 @@
+//obj property access 
+
+const user = {
+    name: "Karan",
+    age: 22,
+    email: "mail@karan.email",
+};
+
+console.log(user.age);
+console.log(user["email"]);
+
+
+
+
+//obj property assignment or creation
+
 //1
 const user = {
     name: "karan",
@@ -33,3 +49,38 @@ const user2 = {
     ...user,
     age: 24
 }
+
+
+// ------------------------------------------------------------
+const name = "email";
+
+// SETTING properties
+const obj1 = {
+    [name]: ''    // Creates { email: '' }
+}
+
+const obj2 = {
+    name: ''      // Creates { name: '' }
+}
+
+// ACCESSING properties
+obj1[name]      // Returns ''
+obj1.email      // Returns ''
+obj2[name]      // Returns undefined
+obj2.name       // Returns ''
+
+
+// -------------------------------------------------
+//prettier-ignore
+/* The square brackets in the object literal[variable] tell JavaScript 
+to use the VALUE of the name variable as the property name,
+    rather than literally using "name" as the property name. */
+
+const variable = "age";
+
+const obj = {
+    [variable]: 24
+}
+
+
+

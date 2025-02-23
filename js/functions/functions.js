4b90abff76b110e874a6daef6cf3edfbebd48811
Karan <xkaranx5@gmail.com>
2025-02-23 09:55:27 +0530

    herarty

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,4 +1,20 @@
 //higher order funcitons
+funtion a(fn){
+    console.log("hello")
+    fn()
+}
+
+funtion b(){
+    console.log("this si function b")
+}
+
+a(b) //passed the function as an argument
+
+a(()=> {  //wrote the function directly
+    console.log("this si function b")
+})
+
+
 
 
 

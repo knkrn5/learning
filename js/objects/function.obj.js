72ce8bf05d12f1be3b7b11c5cd919722c813caa4
Karan <xkaranx5@gmail.com>
2025-03-03 16:36:55 +0530

    ojf val

--- js/objects/function.obj.js
+++ js/objects/function.obj.js
@@ -1,3 +1,5 @@
+/****  confusion between return the function name and object name ****/
+
 //----------------------------------
 const fn = (data) =>{
     console.log(data)
@@ -15,6 +17,13 @@ fn(obj)
 const str = "karan";
 fn(str)
 
+//---------------------
+objfn = (obj) => {
+    name = "karan";
+    age: 21;
+    marks: 90;
+}
+
 //--------------------------------------
 const fn = (obj) => {
     obj.age = 30;  // Modifies the original object
@@ -25,3 +34,36 @@ fn(person);
 
 console.log(person.age); // Output: 30 (original object modified)
 
+
+//----------------------------------------
+/**  we can only assign the value that is being defined **/
+
+const objfn = (obj) => {
+    name = username;  // ❌ Undefined variable 'username'
+    age = age;        // ❌ This does nothing, it's just assigning the same value
+    marks = marks;    // ❌ Same issue as 'age'
+    gender;           // ❌ Undefined variable 'gender'
+}
+
+obj1 = {
+    age: 21,
+    marks: 90,
+}
+
+objfn(obj1)
+
+//------------------------
+
+const objfn = (obj) => {
+    name = username;
+    age = age
+    marks =  marks
+    return obj;
+}
+
+obj1 = {
+    age: 21,
+    marks: 90,
+}
+
+objfn(obj1)
